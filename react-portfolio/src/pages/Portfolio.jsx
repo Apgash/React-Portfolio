955452edf77207f53e415a87998b4f5221a113bf
Apgash <ashtonvilkosi@gmail.com>
2024-11-09 17:50:58 -0500

    adds screenshots to public folder to try and fix deployement bug

--- react-portfolio/src/pages/Portfolio.jsx
+++ react-portfolio/src/pages/Portfolio.jsx
@@ -8,25 +8,25 @@ function Portfolio() {
   const projects = [
     {
       title: 'Piano Player',
-      image: 'src/assets/Screenshot 2024-11-09 125824.png',
+      image: 'public/Screenshot 2024-11-09 125824.png',
       deployedUrl: 'https://apgash.github.io/Piano-Project/',
       githubUrl: 'https://github.com/Apgash/Piano-Project',
     },
     {
       title: 'Vehicle Builder',
-      image: '/src/assets/Screenshot 2024-11-09 140022.png',
+      image: 'public/Screenshot 2024-11-09 140022.png',
       deployedUrl: 'https://github.com/Apgash/Vehicle-Builder',
       githubUrl: 'https://github.com/Apgash/Vehicle-Builder',
     },
     {
       title: 'Employee Maker',
-      image: '/src/assets/Screenshot 2024-11-09 140242.png',
+      image: 'public/Screenshot 2024-11-09 140242.png',
       deployedUrl: 'https://github.com/Apgash/Employee-Maker',
       githubUrl: 'https://github.com/Apgash/Employee-Maker',
     },
     {
       title: 'ReadMe Generator',
-      image: '/src/assets/Screenshot 2024-11-09 140519.png',
+      image: 'public/Screenshot 2024-11-09 140519.png',
       deployedUrl: 'https://github.com/Apgash/Repository-Generator',
       githubUrl: 'https://github.com/Apgash/Repository-Generator',
     },
