e07e994b29d6b620a5c6d874c03fe37bf556064d
Apgash <ashtonvilkosi@gmail.com>
2024-11-08 20:47:28 -0500

    adds code for header.js

---
+++ react-portfolio/src/components/Header.js
@@ -0,0 +1,20 @@
+// Importing React library
+import React from 'react';
+// Importing Navigation component
+import Navigation from './Navigation';
+
+// Defining the Header functional component
+function Header() {
+  return (
+    // Returning a header element
+    <header>
+      {/* Displaying my name */}
+      <h1>Ashton Gardner</h1>
+      {/* Including the Navigation component */}
+      <Navigation />
+    </header>
+  );
+}
+
+// Exporting the Header component as the default export
+export default Header;
