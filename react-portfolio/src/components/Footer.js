d9c29ed2cea2411678bd2d14cbfb5495613fc3a3
Apgash <ashtonvilkosi@gmail.com>
2024-11-08 20:52:51 -0500

    adds notes to footer.js

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -1,11 +1,16 @@
+// Importing React library
 import React from 'react';
 
+// Defining the Footer functional component
 function Footer() {
   return (
+    // Returning a footer element
     <footer>
+      {/* Link to the GitHub profile */}
       <a href="https://github.com/apgash" target="_blank" rel="noopener noreferrer">GitHub</a>
     </footer>
   );
 }
 
+// Exporting the Footer component as the default export
 export default Footer;
