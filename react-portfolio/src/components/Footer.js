c7dff154a5ec5fd97b04029bfc02c96b084d6f59
Apgash <ashtonvilkosi@gmail.com>
2024-11-08 20:52:22 -0500

    adds footer.js code

---
+++ react-portfolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/apgash" target="_blank" rel="noopener noreferrer">GitHub</a>
+    </footer>
+  );
+}
+
+export default Footer;
