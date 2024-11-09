14649d7c9e0b00991f8e7141fa9ae20b14fe4209
Apgash <ashtonvilkosi@gmail.com>
2024-11-09 12:49:39 -0500

    adds code for about me

---
+++ react-portfolio/src/pages/AboutMe.js
@@ -0,0 +1,20 @@
+// Importing React library
+import React from 'react';
+
+// Defining the AboutMe functional component
+function AboutMe() {
+  return (
+    // Returning a section element
+    <section>
+      {/* Displaying the section title */}
+      <h2>About Me</h2>
+      {/* Displaying the developer's avatar */}
+      <img src="/path/to/avatar.jpg" alt="Developer avatar" />
+      {/* Displaying a short bio */}
+      <p>My name is Ashton Gardner and i'm currently an aspiring web developer. I have created this portfolio from scrath to show off and showcase my projects I have created so far!</p>
+    </section>
+  );
+}
+
+// Exporting the AboutMe component as the default export
+export default AboutMe;
