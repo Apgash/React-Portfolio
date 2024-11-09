cd4ebe7d86e389041edef4e6aac0663a59dd968f
Apgash <ashtonvilkosi@gmail.com>
2024-11-09 17:55:06 -0500

    deletes unused screenshots

--- react-portfolio/src/pages/AboutMe.jsx
+++ react-portfolio/src/pages/AboutMe.jsx
@@ -9,7 +9,7 @@ function AboutMe() {
       {/* Displaying the section title */}
       <h2>About Me</h2>
       {/* Displaying the developer's avatar */}
-      <img src="\src\assets\Screenshot 2024-11-09 141710.png" alt="Developer avatar" />
+      <img src="public\Screenshot 2024-11-09 141710.png" alt="Developer avatar" />
       {/* Displaying a short bio */}
       <p>My name is Ashton Gardner and i'm currently an aspiring web developer. I have created this portfolio from scrath to show off and showcase my projects I have created so far!</p>
     </section>
