665302865a69e3baed1cbc98b35bb2a4f5623c2c
Apgash <ashtonvilkosi@gmail.com>
2024-11-09 16:50:53 -0500

    fixes app.jsx bug

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import AboutMe from './pages/AboutMe.jsx';
 import Portfolio from './pages/Portfolio.jsx';
 import Contact from './pages/Contact.jsx';
+import Resume from './pages/Resume.jsx';
 import Footer from './components/Footer.jsx';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      {/* Rendering the Header component */}
-      <Header />
-      {/* Rendering the AboutMe component */}
-      <AboutMe />
-      {/* Rendering the Portfolio component */}
-      <Portfolio />
-      {/* Rendering the Contact component */}
-      <Contact />
-      {/* Rendering the Footer component */}
-      <Footer />
-    </div>
+      <div className="App">
+        {/* Rendering the Header component */}
+        <Header />
+        {/* Defining routes for different pages */}
+        <Routes>
+          <Route path="/" element={<Navigate to="/about" />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+        {/* Rendering the Footer component */}
+        <Footer />
+      </div>
+  
   );
 }
 
